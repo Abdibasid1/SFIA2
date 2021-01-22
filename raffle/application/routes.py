eda09e18b93cf5075e967caf1f6945de80019e8b
Abdibasid1 <basidyaccub43@gmail.com>
2021-01-22 18:22:43 +0000

    new routes for raffle

--- raffle/application/routes.py
+++ raffle/application/routes.py
@@ -4,10 +4,10 @@ from flask import render_template
 
 @app.route('/', methods= ['GET'])
 def index():
-    number_response = requests.get("http://SFIA2_number:5001/number")
-    prize_response = requests.get("http://SFIA2_prize:5002/prize")
+    number_response = requests.get("http://SFIA2_number_backend:5001/number")
+    prize_response = requests.get("http://SFIA2_prize_backend:5002/prize")
     winner_response = requests.post(
-        "http://SFIA2_winner:5003/winner",
+        "http://SFIA2_winner_backend:5003/winner",
         json=dict(number=number_response.text, prize=prize_response.text)
     )
     return render_template("index.html", prize=prize_response.text, number=number_response.text)
